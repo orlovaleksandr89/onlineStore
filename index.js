fffb9e5d8732f6abd2ea8ad19ae133f12d839d5b
Oleksandr orlov <orlovaleksandr89@gmail.com>
2021-10-28 18:13:17 -0700

    deploy to heroku

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
-
+const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
 
 app.use(express.json({ extended: true }))
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+app.use('/', require('./routes/auth.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
