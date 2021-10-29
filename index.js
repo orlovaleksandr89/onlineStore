84a266f6e12d5b43509570f641b17f93a09ec3c6
Oleksandr orlov <orlovaleksandr89@gmail.com>
2021-10-28 18:38:25 -0700

    predeploy

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.json({ extended: true }))
 app.use('/', require('./routes/auth.routes'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.use(express.static(path.join(__dirname, 'build')))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
 
