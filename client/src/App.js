65d67786315d5b99cb70a8a8d318f3326835d120
Oleksandr orlov <orlovaleksandr89@gmail.com>
2021-11-13 16:38:56 -0800

    trying to fix deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState, useCallback } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import './App.css'
 import AppRouter from './components/AppRouter'
 import Layout from './UI/Layout'
@@ -11,7 +11,7 @@ function App() {
   const storeCtx = useContext(StoreContext)
   const { request, error } = useHttp()
   const [isLoading, setIsLoading] = useState(true)
-  const check = useCallback(async () => {
+  const check = async () => {
     try {
       setIsLoading(true)
       const token = localStorage.getItem('token')
@@ -20,7 +20,6 @@ function App() {
         Authorization: `Bearer ${token}`
       })
       const user = jwt_decode(data.token)
-
       storeCtx.setUser(user)
       storeCtx.setAuth(true)
       localStorage.setItem('token', data.token)
@@ -30,27 +29,30 @@ function App() {
       storeCtx.setError(e)
       console.log(e)
     }
-  }, [request, storeCtx])
+  }
 
-  const getTypesAndItemsFromDB = useCallback(async () => {
+  const getTypesAndItemsFromDB = async () => {
     try {
       const items = await request('/items')
       const types = await request('/types')
       storeCtx.setTypes(types)
       storeCtx.setItems(items)
     } catch (error) {}
-  }, [request, storeCtx])
+  }
 
   useEffect(() => {
     isLoading && check()
     return () => setIsLoading(false)
   }, [])
+
   useEffect(() => {
     getTypesAndItemsFromDB()
   }, [])
+
   if (isLoading) {
     return <Loader />
   }
+
   return (
     <Layout>
       <AppRouter error={error} />
