72196177359286c2da2cbed3d90a955eed9400c5
Oleksandr orlov <orlovaleksandr89@gmail.com>
2021-12-23 17:07:58 -0800

    heroku deploy dec 23

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/auth/cart', require('./routes/cart.routes'))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')))
 
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
